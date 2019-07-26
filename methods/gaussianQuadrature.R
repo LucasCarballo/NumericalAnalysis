6ee54b6692366bf538063cbd1e1953ca4b5512e9
Lucas Carballo <lucas.d.carballo@gmail.com>
2019-07-25 22:36:55 -0300

    adds gaussian quadrature error

--- methods/gaussianQuadrature.R
+++ methods/gaussianQuadrature.R
@@ -1,3 +1,5 @@
+require('orthopolynom')
+
 weights <- function(p, x, n) {
   (2*(1-x^2))/(n^2*p(x)^2)
 }
@@ -16,5 +18,11 @@ legendreCoeficients <- function(n) {
 gaussianQuadrature <- function(f, a, b, n) {
   lCoeficients <- legendreCoeficients(n)
 
-  (b-a)/2 * sum(lCoeficients[1:n, 2] * f(((b-a) * lCoeficients[1:n, 1] + b + a)/2))
+  f2ndx <- Deriv(f, 'x', nderiv = (2*n))
+  
+  error <- (((b-a)^(2*n+1) * fact(n)^4)/((2*n+1)*((fact(2*n)^3)))) * fminbnd(f2ndx, a, b, maximum = TRUE)[[2]]  
+  
+  g <- (b-a)/2 * sum(lCoeficients[1:n, 2] * f(((b-a) * lCoeficients[1:n, 1] + b + a)/2))
+  
+  c(g, error)
 }
